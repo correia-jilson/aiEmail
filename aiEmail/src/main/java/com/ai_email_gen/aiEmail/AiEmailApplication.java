197e74deb16b10314e3a37b9e79bea5d189fa3c4
correia.ji <correia.ji@northeastern.edu>
2025-02-11 19:39:47 -0500

    initialising springboot project with springinitialiser

---
+++ aiEmail/src/main/java/com/ai_email_gen/aiEmail/AiEmailApplication.java
@@ -0,0 +1,13 @@
+package com.ai_email_gen.aiEmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiEmailApplication.class, args);
+	}
+
+}
---
+++ aiEmail/src/test/java/com/ai_email_gen/aiEmail/AiEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai_email_gen.aiEmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
